f4971ad36cfa5baf0c1addded2ac9aa1128b697c
GingerBear <bbbear444@gmail.com>
2017-04-14 10:32:13 -0400

    floating window working

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const globalShortcut = electron.globalShortcut
+const Menu = electron.Menu
 
 const path = require('path')
 const url = require('url')
@@ -11,9 +13,9 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -22,11 +24,34 @@ function createWindow () {
     slashes: true
   }))
 
+
+  const shortcut = globalShortcut.register('Control+Space', () => {
+    if (mainWindow.isVisible()){
+      app.hide();
+    } else {
+      app.show();
+      mainWindow.show();
+    }
+  });
+
+
+  // const shortcut2 = globalShortcut.register('Esc', () => {
+  //   app.hide();
+  // });
+
+  if (!shortcut) { console.log('Registration failed.'); }
+  // if (!shortcut2) { console.log('Registration 2 failed.'); }
+
+  // mainWindow.on('close', (event) => {
+  //   event.preventDefault();
+  //   mainWindow.hide();
+  // });
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -37,10 +62,42 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow();
+
+
+const template = [{
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Hide',
+        accelerator: 'Esc',
+        click () { app.hide(); }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
+})
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -48,7 +105,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -58,3 +115,4 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
